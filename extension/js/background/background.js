7705bb889d9fdfc1f7033300254ad953f02ee775
Masamitsu MURASE <masamitsu.murase@gmail.com>
2022-12-19 01:45:12 +0900

    Fix errors.

--- extension/js/background/background.js
+++ extension/js/background/background.js
@@ -1,19 +1,3 @@
-const other_scripts = [
-    "js/common/misc.js",
-    "js/common/action_matcher.js",
-    "js/common/misc_settings.js",
-    "js/common/error.js",
-    "js/common/data_migrator.js",
-    "js/common/svn_url.js",
-    "js/background/badge_manager.js",
-    "js/background/context_menu.js",
-    "js/background/message_receiver.js",
-    "js/background/tortoise_svn_opener.js",
-    // "js/background/background.js"
-];
-const full_paths = other_scripts.map(path => chrome.runtime.getURL(path));
-importScripts(...full_paths);
-
 var OpenTsvn;
 if (!OpenTsvn) OpenTsvn = {};
 
